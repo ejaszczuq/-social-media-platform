97f0a788fee1bb317511e2d3def050a161d7321d
ejaszczuq <ewelina.jaszczuk13006@gmail.com>
2024-04-09 20:19:35 +0200

    create LogInBox component, modify LogInPage and styles

--- src/components/LogInBox/LogInBox.tsx
+++ src/components/LogInBox/LogInBox.tsx
@@ -9,14 +9,36 @@ const LogInBox: React.FC<ILogInBox> = () => {
       <div className="log-in-box">
          <h1>Log in</h1>
          <h3>to start creating</h3>
-         <label htmlFor="userNameInp">
-            <input type="text" id="userNameInp" name="userNameInp" />
-         </label>
-         <label htmlFor="passwordInp">
-            <input type="password" id="passwordInp" name="passwordInp" />
-         </label>
+         <div className="inputs">
+            <label htmlFor="userNameInp">
+               Username
+               <input type="text" id="userNameInp" name="userNameInp" />
+            </label>
+            <label htmlFor="passwordInp">
+               Password
+               <input type="password" id="passwordInp" name="passwordInp" />
+            </label>
+         </div>
          <p>Forgot password?</p>
-         <button>Log in</button>
+         <button>
+            Log in
+            <svg
+               xmlns="http://www.w3.org/2000/svg"
+               width="24"
+               height="24"
+               viewBox="0 0 24 24"
+               fill="none"
+               stroke="currentColor"
+               stroke-width="2"
+               stroke-linecap="round"
+               stroke-linejoin="round"
+            >
+               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+               <path d="M5 12l14 0" />
+               <path d="M13 18l6 -6" />
+               <path d="M13 6l6 6" />
+            </svg>
+         </button>
       </div>
    );
 };
