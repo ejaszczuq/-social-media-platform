ffb210323d6dcf3e0015ebe165cf60d5c50a3ee1
ejaszczuq <ewelina.jaszczuk13006@gmail.com>
2024-04-09 17:38:15 +0200

    add background

--- src/app/Login/Login.tsx
+++ src/app/Login/Login.tsx
@@ -6,7 +6,6 @@ import LogInBox from '../../components/LogInBox/LogInBox';
 const LoginPage = () => {
    return (
       <div className="login">
-         <h1>Login Page</h1>
          <LogInBox />
       </div>
    );
